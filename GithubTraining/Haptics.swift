d33aaf46da84f0be405fe61bf6228bc412db5301
taichi <positive703@gmail.com>
2021-09-30 06:40:22 +0900

    [feat] Haptics now got three features. three kind of Haptics can now access through the Haptics.

--- GithubTraining/Haptics.swift
+++ GithubTraining/Haptics.swift
@@ -11,4 +11,19 @@ struct Haptics {
     static let notification: UINotificationFeedbackGenerator = UINotificationFeedbackGenerator()
     static let impact: UIImpactFeedbackGenerator = UIImpactFeedbackGenerator()
     static let selection: UISelectionFeedbackGenerator = UISelectionFeedbackGenerator()
+    
+    static func simpleSuccess() {
+        notification.prepare()
+        notification.notificationOccurred(.success)
+    }
+    
+    static func simpleImpact() {
+        impact.prepare()
+        impact.impactOccurred(intensity: 0.6)
+    }
+    
+    static func simpleSelection() {
+        selection.prepare()
+        selection.selectionChanged()
+    }
 }
