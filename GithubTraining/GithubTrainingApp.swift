4f68a0cfeade72feef71a86c0c9e3093079a6e20
taichi <positive703@gmail.com>
2021-09-30 04:46:49 +0900

    Initial Commit

---
+++ GithubTraining/GithubTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTrainingApp.swift
+//  GithubTraining
+//
+//  Created by Taichi Arima on 2021/09/30.
+//
+
+import SwiftUI
+
+@main
+struct GithubTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
