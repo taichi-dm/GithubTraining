a28e3027ae0c3dded8a7fee29d086c2585a7b8e2
taichi <positive703@gmail.com>
2021-09-30 07:17:42 +0900

    [feat] New SecondView has been arrived! I will edit this View next!

---
+++ GithubTraining/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  GithubTraining
+//
+//  Created by Taichi Arima on 2021/09/30.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
