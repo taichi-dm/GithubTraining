31724859fdcef6115c419bf1af6f3f0c054fb351
taichi <positive703@gmail.com>
2021-09-30 07:43:17 +0900

    [docs] removed comment line about pull request.

--- GithubTraining/ContentView.swift
+++ GithubTraining/ContentView.swift
@@ -11,7 +11,6 @@ struct ContentView: View {
     @StateObject private var viewModel: ViewModel = ViewModel()
     
     // I'm tryna tag feature! I hope this works great.
-    // Let's try pull request!
     
     var body: some View {
         NavigationView {
